0a3cbf14b07a9e78407474ce2fc968044ed49dfd
Leigh Hobson <leighhobson89@aol.com>
2018-03-29 23:00:12 +0200

    VERY Basic Login System v1.0

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-var name = "Leigh";
-document.getElementById("leigh").innerHTML = "Hello " + name;
+function login(userName, pass) {
+    var users = [{user: "leigh1981", password: "Burnleyfc81"},{user: "leigh1982", password: "Burnleyfc82"}];
+    var valid = null;
+    for (i=0;i<users.length;i++) {
+        if (users[i].user == userName && users[i].password == pass) {
+            valid = 1;
+            break;
+        }
+        else {
+            continue;
+        }
+    }
+    if (valid == 1) {
+        alert("Thanks " + userName + ", you are logged in!");
+    }
+    else {
+        alert("Invalid User!");
+    }
+}
